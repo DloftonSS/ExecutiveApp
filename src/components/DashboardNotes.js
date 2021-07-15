b6cd0b509a2cd146afbeb98765955527f84931bb
DloftonSS <derek.lofton1@gmail.com>
2021-07-15 15:44:21 -0400

    notes

--- src/components/DashboardNotes.js
+++ src/components/DashboardNotes.js
@@ -54,7 +54,7 @@ function DashboardNotes() {
                       >
                         Name of Admin
                       </Feed.User>
-                      <Feed.Date>{notesList[keyName].createdAt}</Feed.Date>
+                      <Feed.Date></Feed.Date>
                     </Feed.Summary>
                     <Feed.Meta>
                       <Feed.User>Name of Customer</Feed.User>
