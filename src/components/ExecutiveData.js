344dff834cc4dff6ba44cb5d920e690c3a866333
DloftonSS <derek.lofton1@gmail.com>
2021-11-19 09:35:18 -0500

    source

--- src/components/ExecutiveData.js
+++ src/components/ExecutiveData.js
@@ -1503,7 +1503,7 @@ function ExecutiveData(props) {
                           <Icon name="edit outline"></Icon>
                         </Button>
                       </Table.Cell>
-                      {/* <Table.Cell>
+                      <Table.Cell>
                         <textarea
                           onChange={(e) => {
                             setNewSource(e.target.value);
@@ -1519,7 +1519,7 @@ function ExecutiveData(props) {
                           Update
                           <Icon name="edit outline"></Icon>
                         </Button>
-                      </Table.Cell> */}
+                      </Table.Cell>
 
                       <Table.Cell>{update}</Table.Cell>
                       {/* <Table.Cell></Table.Cell> */}
